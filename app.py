01ec8c5ad1e526e4f77e7ff9a3603a72e9d89bd0
Siddhesh <mungekarsiddhesh2001@gmail.com>
2022-04-15 02:05:46 +0530

    intial commit basic frontend made

---
+++ app.py
@@ -0,0 +1,34 @@
+# create flask app
+from flask import Flask, request
+from flask.templating import render_template
+import joblib
+app = Flask(__name__)
+
+model = joblib.load("./model_joblib")
+@app.route("/", methods=["GET", "POST"])
+def index():
+    if request.method == "GET":
+        return render_template("index.html")
+    if request.method == "POST":
+        # get the data from the form
+        data = request.form
+        geolocation = int(data.get("geolocation"))
+        gender = int(data.get("gender"))
+        creditScore = int(data.get("credit-score"))
+        age = int(data.get("age"))
+        tenure = int(data.get("tenure"))
+        balance = int(data.get("balance"))
+        numOfProducts = int(data.get("numOfProducts"))
+        hasCrCard = int(data.get("hasCrCard"))
+        isActiveMember = int(data.get("isActiveMember"))
+        estimatedSalary =  float(data.get("estimatedSalary"))
+        germany = 1 if geolocation == 2 else 0
+        spain = 1 if geolocation == 1 else 0
+        output = model.predict([[creditScore, age, tenure, balance, numOfProducts, hasCrCard, isActiveMember, estimatedSalary, germany, spain, gender]]),
+        # print(creditScore, age, tenure, balance, numOfProducts, hasCrCard, isActiveMember, germany, spain, gender)
+        # make prediction
+        print(output[0][0])
+        return "will leave company" if output[0][0]==1 else "will not leave company"
+
+if __name__ == "__main__":
+    app.run(debug=True)
