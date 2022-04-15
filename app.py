b716272ea84d35a8ad159cbba9758fc5b81fc849
bhaveshmhadse <bhaveshmhadse9@gmail.com>
2022-04-15 21:01:03 +0530

    Added Styles and refactored the code

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
-# create flask app
 from flask import Flask, request
 from flask.templating import render_template
 import joblib
+
 app = Flask(__name__)
 
 model = joblib.load("./model_joblib")
+
+
 @app.route("/", methods=["GET", "POST"])
 def index():
     if request.method == "GET":
@@ -21,14 +23,36 @@ def index():
         numOfProducts = int(data.get("numOfProducts"))
         hasCrCard = int(data.get("hasCrCard"))
         isActiveMember = int(data.get("isActiveMember"))
-        estimatedSalary =  float(data.get("estimatedSalary"))
+        estimatedSalary = float(data.get("estimatedSalary"))
         germany = 1 if geolocation == 2 else 0
         spain = 1 if geolocation == 1 else 0
-        output = model.predict([[creditScore, age, tenure, balance, numOfProducts, hasCrCard, isActiveMember, estimatedSalary, germany, spain, gender]]),
+        output = (
+            model.predict(
+                [
+                    [
+                        creditScore,
+                        age,
+                        tenure,
+                        balance,
+                        numOfProducts,
+                        hasCrCard,
+                        isActiveMember,
+                        estimatedSalary,
+                        germany,
+                        spain,
+                        gender,
+                    ]
+                ]
+            ),
+        )
         # print(creditScore, age, tenure, balance, numOfProducts, hasCrCard, isActiveMember, germany, spain, gender)
+
         # make prediction
         print(output[0][0])
-        return "will leave company" if output[0][0]==1 else "will not leave company"
+
+        isUpToMark = output[0][0] == 1
+        return "Will not leave company" if isUpToMark else "Will leave company"
+
 
 if __name__ == "__main__":
     app.run(debug=True)
